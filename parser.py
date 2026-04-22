198bf37e28e35e8c766b4f549fba114625c5b15d
BRAYC1330 <256725968+BRAYC1330@users.noreply.github.com>
2026-04-22 22:08:11 +0000

    Update parser.py

--- parser.py
+++ parser.py
@@ -2,26 +2,21 @@ import logging
 from typing import List, Dict, Optional, TypedDict
 import utils
 from logging_config import setup_logging
-
 setup_logging()
 logger = logging.getLogger(__name__)
-
 class PostNode(TypedDict):
     uri: str
     cid: str
     handle: str
     text: str
     is_root: bool
-
 THREAD_CACHE: Dict[str, List[PostNode]] = {}
-
 async def parse_thread(thread_data: dict, root_uri: str = "", client=None):
     logger.debug(f"[parse_thread] Parsing thread data")
     nodes = utils.flatten_thread(thread_data.get("thread", {}))
     logger.debug(f"[parse_thread] Extracted {len(nodes)} nodes")
     return nodes
-
-async def parse_digest_thread(thread_data: Dict) -> Dict:
+async def parse_digest_thread(thread_ Dict) -> Dict:
     logger.debug(f"[parse_digest_thread] Parsing thread with {len(thread_data.get('thread', {}))} nodes")
     nodes = utils.flatten_thread(thread_data.get("thread", {}))
     root = next((n for n in nodes if n.get("is_root")), {"uri": "", "cid": "", "text": ""})
