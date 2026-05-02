662dc07e5388d18461bfa44b6bb4da052d2b060a
BRAYC1330 <256725968+BRAYC1330@users.noreply.github.com>
2026-05-02 08:13:35 +0000

    Delete src directory

--- src/clients/httpx_base.py
+++
@@ -1,6 +0,0 @@
-import httpx
-
-def get_client():
-    limits = httpx.Limits(max_connections=20, max_keepalive_connections=5)
-    timeout = httpx.Timeout(connect=10.0, read=30.0, write=30.0, pool=10.0)
-    return httpx.AsyncClient(limits=limits, timeout=timeout)
